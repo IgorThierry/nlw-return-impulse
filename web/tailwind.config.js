64f1c775b12f5360413c403b3e153ee2ad2eefde
Igor Thierry <igorthierry15@gmail.com>
2022-05-02 09:49:41 -0300

    feat: Widget Button Component

---
+++ web/tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          500: "#8257e6",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
